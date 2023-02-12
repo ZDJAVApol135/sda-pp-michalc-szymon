fc59b6b1e37ffd48c30ff4d389cb6a364324e068
Michał Chmielewski <chmielewski.michal007@gmail.com>
2023-02-12 15:58:51 +0100

    TASK 22

--- src/main/java/com/sda/service/UsersService.java
+++ src/main/java/com/sda/service/UsersService.java
@@ -1,9 +1,13 @@
 package com.sda.service;
 
 import com.sda.dao.UsersDAO;
+import com.sda.dto.UserDTO;
 import com.sda.mapper.UserMapper;
-import lombok.AllArgsConstructor;
+import com.sda.model.User;
 import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequiredArgsConstructor // tworzy konstruktory argumentowe tylko dla pól finalnych
@@ -11,4 +15,19 @@ public class UsersService {
 
     private final UsersDAO usersDAO;
     private final UserMapper userMapper;
+
+    public List<UserDTO> findAll() {
+//        List<User> users = usersDAO.findAll();
+//        List<UserDTO> userDTOS = new ArrayList<>();
+//
+//        for (User user : users) {
+//            UserDTO dto = userMapper.map(user);
+//            userDTOS.add(dto);
+//        }
+//        return userDTOS;
+        return usersDAO.findAll().stream()
+                .map(user -> userMapper.map(user))
+                .toList();
+    }
+
 }
