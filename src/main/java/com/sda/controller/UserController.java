ea127dda25825629e64a68611d66654a685bcdc8
Michał Chmielewski <chmielewski.michal007@gmail.com>
2023-02-18 11:30:48 +0100

    task 27

--- src/main/java/com/sda/controller/UserController.java
+++ src/main/java/com/sda/controller/UserController.java
@@ -1,12 +1,25 @@
 package com.sda.controller;
 
+import com.sda.dto.UserDTO;
 import com.sda.service.UsersService;
+import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 public class UserController {
 
     private final UsersService userService;
 
-    public UserController(UsersService userService) {
-        this.userService = userService;
+    public void findAll(){
+        List<UserDTO> all = userService.findAll();
+        if (all == null){
+            System.out.println("Users list empty!");
+        }
+        System.out.println(all);
+    }
+
+    public void findByUsername(String username){
+
     }
 }
